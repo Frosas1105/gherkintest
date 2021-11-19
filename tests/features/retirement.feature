6fb65594489eac863a076a25d5e620f32c0946cc
Fabian Rosas <frosas@my.waketech.edu>
2021-11-19 11:40:50 -0500

    2nd commit with 2 scenarios

--- tests/features/retirement.feature
+++ tests/features/retirement.feature
@@ -1,6 +1,10 @@
 Feature: retirement calc
-Checking If assertion is raised if birth year is under 1900
+    Checking If assertion are raised
 
 Scenario: Birth year is under 1900
-Given birth year entered is under 1900
-Then assertion statement is raised
+    Given birth year entered is under 1900
+    Then assertion statement is raised
+
+Scenario: Birth year is over 3000
+    Given birth year entered is over 3000
+    Then assertion statement is raised
--- tests/step_defs/test_retirement_steps.py
+++ tests/step_defs/test_retirement_steps.py
@@ -1,18 +1,33 @@
-from pytest_bdd import scenario, given, when, then
-
+from pytest_bdd import scenarios, given, when, then, parsers
+from pytest_bdd.scenario import scenario
 from retirement import *
 
-@scenario('../features/retirement.feature', 'Birth year is under 1900')
+CONVERTERS = {
+    'dates': int,
+}
+
+scenarios('../features/retirement.feature')
+
 
 def test_birthyear():
     pass
 
 @given('birth year entered is under 1900')
-def input():
+def test_input():
+   pass
+    
+@then('assertion statement is raised')
+def test_input1899():
+    with pytest.raises(ValueError):
+        calculate_retirement_age(1899)
+    
+@given('birth year entered is over 3000')
+def test_input2():
     pass
 
-
 @then('assertion statement is raised')
-def input3000():
+def test_input3001():
     with pytest.raises(ValueError):
-        calculate_retirement_age(3000)
+        calculate_retirement_age(3001)
+
+    
