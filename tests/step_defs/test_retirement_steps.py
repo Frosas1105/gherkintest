a464059ae7abdb71a5ecb643d4fa54f2763897e8
Fabian Rosas <frosas@my.waketech.edu>
2021-11-19 14:02:44 -0500

    commit before travis test

--- tests/step_defs/test_retirement_steps.py
+++ tests/step_defs/test_retirement_steps.py
@@ -1,6 +1,7 @@
 import pytest
 from pytest_bdd import scenarios, given, when, then
 from SScalcRosas import *
+from ssModule2 import *
 
 EXTRA_TYPES = {
     'Number': int
@@ -15,19 +16,18 @@ CONVERTERS = {
 scenarios('../features/retirement.feature', example_converters=CONVERTERS)
 
 @given('the user has opened the app')
-def step_function():
+def start():
     pass
 
 @when('the <"birthYear>" is entered by the user')
-def step_function(birthYear):
+def birthYear(birthYear):
     return birthYear
 
 @when('the <"birthMonth>" is entered by the user')
-def step_function(birthMonth):
-    # Add Your Code Here
-    pass
+def birthMonth(birthMonth):
+    return birthMonth
 
-@then(Parser('the age of retirement is "<retireAge>" and the month of retirement is "<retireMonthNum>"'))
-def step_function(session: Session):
-    # Add Your Code Here
-    pass
+@then('the age of retirement is "<retireAge>" and the month of retirement is "<retireMonthNum>"')
+def step_function(birthYear, birthMonth, retireAge, retireMonthNum):
+   year, month, age, monthNum = myFunc2(birthYear, birthMonth, retireAge, retireMonthNum)
+   assert year == birthYear and month == birthMonth and age == retireAge and monthNum == retireMonthNum
